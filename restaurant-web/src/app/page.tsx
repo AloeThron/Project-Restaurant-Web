cac3a7c4c8ac74b0647e068c46ccdce553e7ff9e
AloeThron <lxe.tr.deat@gmail.com>
2024-01-05 02:45:24 +0700

    setting nav menu

---
+++ restaurant-web/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
+import Reservation from "@/components/Reservation";
+import StyleGuide from "@/components/StyleGuide";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
+      <Header />
+      <Hero />
+      <Menu />
+      <Reservation />
+      {/* <StyleGuide /> */}
+      <div className="h-[4000px]"></div>
+    </main>
+  );
+}
